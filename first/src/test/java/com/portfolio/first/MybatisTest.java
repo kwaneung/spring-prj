8da33220344a09f7c21f59f6588fb454e02f768a
kwaneung <kwaneung.kim@outlook.com>
2020-07-14 20:59:40 +0900

    DB 셋팅 및 접속 테스트
    mysql 8 버전 기준 사용자mess 비밀번호 mess.
    localhost 환경
    mysql 8버전과 mariadb 10.3버전이 호환되므로 추후 확인 필요

---
+++ first/src/test/java/com/portfolio/first/MybatisTest.java
@@ -0,0 +1,36 @@
+package com.portfolio.first;
+
+import javax.annotation.Resource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import javax.inject.Inject;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring/dataSource-context.xml" })
+public class MybatisTest {
+	private static final Logger logger = LoggerFactory.getLogger(MybatisTest.class);
+	
+	@Inject
+	private SqlSessionFactory sessionFactory;
+	
+	@Test
+	public void testSessionFactory() {
+		logger.info("\n Session Factory : " + sessionFactory);
+	}
+	
+	@Test
+	public void testSqlSession() {
+		try (SqlSession session = sessionFactory.openSession()){
+			logger.info("\n Sql Session : " + session);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
