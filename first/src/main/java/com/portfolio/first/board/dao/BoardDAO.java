80c9a4f09de2da147ff90a1573c10416b59a54f3
kwaneung <kwaneung.kim@outlook.com>
2020-07-16 20:37:52 +0900

    DB 테이블 접근
    DB 테이블 생성 후 접근 테스트.
    테이블 명 대소문자 확인

---
+++ first/src/main/java/com/portfolio/first/board/dao/BoardDAO.java
@@ -0,0 +1,37 @@
+package com.portfolio.first.board.dao;
+
+import java.util.List;
+
+
+
+import com.portfolio.first.board.model.BoardVO;
+
+
+
+public interface BoardDAO {
+
+
+
+	public List<BoardVO> getBoardList() throws Exception;
+
+	
+
+	public BoardVO getBoardContent(int bid) throws Exception;
+
+	
+
+	public int insertBoard(BoardVO boardVO) throws Exception;
+
+	
+
+	public int updateBoard(BoardVO boardVO) throws Exception;
+
+	
+
+	public int deleteBoard(int bid) throws Exception;
+
+	
+
+	public int updateViewCnt(int bid) throws Exception;
+
+}
