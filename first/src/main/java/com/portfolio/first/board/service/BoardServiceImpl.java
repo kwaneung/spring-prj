fd38245362066bac28d41bf0f64b36fae7212484
kwaneung <kwaneung.kim@outlook.com>
2020-08-02 22:05:27 +0900

    예외처리

--- first/src/main/java/com/portfolio/first/board/service/BoardServiceImpl.java
+++ first/src/main/java/com/portfolio/first/board/service/BoardServiceImpl.java
@@ -1,10 +1,9 @@
 package com.portfolio.first.board.service;
 
 import java.util.List;
-
 import javax.inject.Inject;
-
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.portfolio.first.board.dao.BoardDAO;
 import com.portfolio.first.board.model.BoardVO;
@@ -30,27 +29,28 @@ public class BoardServiceImpl implements BoardService{
 		boardDAO.updateBoard(boardVO);
 	}
 	
-//	public BoardVO getBoardContent(int bid) throws Exception{
-//		boardDAO.updateViewCnt(bid);
-//		return boardDAO.getBoardContent(bid);
-//	}
-	
-	@Override
+	@Transactional
 	public BoardVO getBoardContent(int bid) throws Exception{
-		BoardVO boardVO = new BoardVO();
-		    
-
 		boardDAO.updateViewCnt(bid);
-	//	boardVO = boardDAO.getBoardContent(bid);
-		try {
-			boardVO.setBid(bid);
-			boardVO.setCate_cd("1111111111111111111111111111111111111");
-			boardDAO.updateBoard(boardVO);
-		} catch (RuntimeException e) {
-			throw new NotFoundException();
-		}
-		return boardVO;
+		return boardDAO.getBoardContent(bid);
 	}
+	
+//	@Transactional
+//	@Override
+//	public BoardVO getBoardContent(int bid) throws Exception{
+//		BoardVO boardVO = new BoardVO();
+//		    
+//		boardDAO.updateViewCnt(bid);
+//	//	boardVO = boardDAO.getBoardContent(bid);
+//		try {
+//			boardVO.setBid(bid);
+//			boardVO.setCate_cd("1111111111111111111111111111111111111");
+//			boardDAO.updateBoard(boardVO);
+//		} catch (RuntimeException e) {
+//			throw new NotFoundException();
+//		}
+//		return boardVO;
+//	}
 	
 	@Override
 	public void deleteBoard(int bid) throws Exception {
