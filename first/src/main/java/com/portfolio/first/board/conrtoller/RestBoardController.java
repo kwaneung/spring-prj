a3f7cdea63da2116ddbd773f184a5c74ea5488e5
kwaneung <kwaneung.kim@outlook.com>
2020-08-23 16:53:22 +0900

    댓글 쓰기 수정 삭제
    삭제 시 reload
    <dependency>
    <groupId>com.google.code.gson</groupId>
    <artifactId>gson</artifactId>
    <version>2.8.5</version>
    </dependency>
    
    추가

--- first/src/main/java/com/portfolio/first/board/conrtoller/RestBoardController.java
+++ first/src/main/java/com/portfolio/first/board/conrtoller/RestBoardController.java
@@ -1,16 +1,16 @@
 package com.portfolio.first.board.conrtoller;
 
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
 import javax.inject.Inject;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.portfolio.first.board.model.ReplyVO;
 import com.portfolio.first.board.service.BoardService;
 
@@ -23,8 +23,50 @@ public class RestBoardController {
 	@Inject
 	private BoardService boardService;
 	
-	@RequestMapping(value = "/getReqlyList", method = RequestMethod.POST)
+	@RequestMapping(value = "/getReplyList", method = RequestMethod.POST)
 	public List<ReplyVO> getReplyList(@RequestParam("bid") int bid) throws Exception {
 		return boardService.getReplyList(bid);
+	}
+	
+	@RequestMapping(value="/saveReply", method = RequestMethod.POST)
+	public Map<String, Object> saveReply(@RequestBody ReplyVO replyVO) throws Exception{
+		Map<String, Object> result = new HashMap<>();
+		
+		try {
+			boardService.saveReply(replyVO);
+			result.put("status", "OK");
+		} catch(Exception e) {
+			e.printStackTrace();
+			result.put("status", "FALSE");
+		}
+		return result;
+	}
+	
+	@RequestMapping(value = "/updateReply", method = {RequestMethod.GET, RequestMethod.POST})
+	public Map<String, Object> updateReply(@RequestBody ReplyVO replyVO) throws Exception {
+		Map<String, Object> result = new HashMap<>();
+		
+		try {
+			boardService.updateReply(replyVO);
+			result.put("status", "OK");
+		} catch (Exception e) {
+			e.printStackTrace();
+			result.put("status", "False");
+		}
+		return result;
+	}
+	
+	@RequestMapping(value = "/deleteReply", method = {RequestMethod.GET, RequestMethod.POST})
+	public Map<String, Object> deleteReply(@RequestParam("rid") int rid) throws Exception {
+		Map<String, Object> result = new HashMap<>();
+		
+		try {
+			boardService.deleteReply(rid);
+			result.put("status", "OK");
+		} catch (Exception e) {
+			e.printStackTrace();
+			result.put("status", "False");
+		}
+		return result;
 	}
 }
